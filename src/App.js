90ce3788050ece461828632ff6da6b468afa4f54
ineggapps <inegg.apps@gmail.com>
2020-02-13 22:09:53 +0900

    프로젝트 정리

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+const Test = styled.p`
+  font-size: 14px;
+  font-weight: bold;
+  color: #ff0000;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test>Hello, World</Test>
     </div>
   );
 }
