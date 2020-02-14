854d762e52bc62c21008d1fd2d602fb1482e1d16
inegg <inegg.apps@gmail.com>
2020-02-14 17:46:38 +0900

    컴포넌트 생성

---
+++ src/Components/Gnb.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Gnb = () => (
+  <div>
+    <ul>
+      <li>Introduce</li>
+      <li>Portfolio</li>
+      <li>Resume</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+);
+
+export default Gnb;
