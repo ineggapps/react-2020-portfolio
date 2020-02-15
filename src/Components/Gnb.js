ff526fcb49024be3f1ba526186b2db6df15701cb
inegg <inegg.apps@gmail.com>
2020-02-15 15:35:42 +0900

    페이지 매핑

--- src/Components/Gnb.js
+++ src/Components/Gnb.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Gnb = () => (
   <div>
     <ul>
-      <li>Introduce</li>
-      <li>Portfolio</li>
+      <li>
+        <Link to={"/introduce"}>Introduce</Link>
+      </li>
+      <li>
+        <Link to={"/portfolio"}>Portfolio</Link>
+      </li>
       <li>Resume</li>
       <li>Contact</li>
     </ul>
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Intro from "../Routes/Intro";
+import Portfolio from "../Routes/Portfolio";
+
+//
+export const PAGE_INTRO = "/introduce";
+export const PAGE_PORTFOLIO = "/portfolio";
 
 const NormalRoutes = () => (
   <Switch>
-    <Route path="/" component={Intro} />
+    <Route path={PAGE_INTRO} component={Intro} />
+    <Route path={PAGE_PORTFOLIO} component={Portfolio} />
   </Switch>
 );
 const Routes = () => (
